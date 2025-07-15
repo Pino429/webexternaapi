88c48df78a6eaf839e39b2c111b4b609f252e449
Pino429 <wmarcelpino@gmail.com>
2025-07-14 21:56:23 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,5 @@
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-app.MapGet("/", async (HttpContext context) =>
-{
-    using var http = new HttpClient();
-    var json = await http.GetStringAsync("https://tudominio/api/notificacion/obtener");
-    context.Response.ContentType = "application/json";
-    await context.Response.WriteAsync(json);
-});
+
+app.MapGet("/", () => "¡Web externa funcionando correctamente!");
 app.Run();
