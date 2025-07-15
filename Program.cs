c7ee33818da4b1a78990465ecc6281fdcbc4e2f9
Pino429 <wmarcelpino@gmail.com>
2025-07-14 22:11:33 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,23 @@
+using System.Net.Http;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "¡Web externa funcionando correctamente!");
+
+app.MapGet("/notificaciones", async () =>
+{
+    using var httpClient = new HttpClient();
+
+    var response = await httpClient.GetAsync("https://gonpin.com/api/notificaciones");
+    if (!response.IsSuccessStatusCode)
+    {
+        return Results.Problem("No se pudo obtener la información de la API externa.");
+    }
+
+    var json = await response.Content.ReadAsStringAsync();
+
+    return Results.Content(json, "application/json");
+});
+
 app.Run();
